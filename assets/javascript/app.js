eb6874739de44b562e5c318bdcd986a8afa2e3cc
Melvin Hernandez <melher39@gmail.com>
2018-11-09 16:41:04 -0600

    gifs are pausing and moving now! also added button for form submit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,21 +2,22 @@ $(document).ready(function(){
 //declare global variables
 // this array will be skateboarding based including professional skaters, tricks, magazines, sites, brands...
 var topics = ["Chris Cole", "Shane O'neill", "Mike Mo Capaldi", "Nyjah Juston", "Paul Rodriguez", "Tony Hawk",
-"Rodney Mullen", "Jamie Thomas", "PJ Ladd", "Kickflip", "Ollie", "360 Flip", "Zero Skateboards", "Thrasher Mag"]
+"Rodney Mullen", "Jamie Thomas", "PJ Ladd", "Chris Joslin", "Kickflip", "Ollie", "360 Flip", "Lazer Flip",
+"Zero Skateboards", "Thrasher Mag", "Nike SB", "Baker Skateboards"]
 
-// var APIkey = "QPQXb6tfP8EP1P1Ci0aJnPiFOUxjDuj5";
-
-// var searchTerm;
 
 function initialButtons(){
-    for (var i=0; i<topics.length ; i++){
+    for (var i=0; i < topics.length ; i++){
         var newBtn = $("<button>");
         newBtn.addClass("btn btn-success initial-buttons");
         newBtn.attr("data-filter", topics[i]);
         newBtn.text(topics[i]);
         $("#button-section").append(newBtn);
     }
+
+    $()
 };
+
 
 function searchGiphy() {
 
@@ -25,6 +26,7 @@ function searchGiphy() {
     var limit = 10;
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + buttonValue +"&api_key=QPQXb6tfP8EP1P1Ci0aJnPiFOUxjDuj5" + "&limit=" + limit;
     console.log(queryURL);
+    $("#gif-section").empty();
 
 $.ajax({
     url: queryURL,
@@ -32,22 +34,49 @@ $.ajax({
 }).then(function(response){
 
     var results = response.data;
-    for (var j=0; j<results.length; j++) {
+
+    for (var j=0; j < results.length; j++) {
     var rating = results[j].rating;
     var p = $("<h6>").text("Rating: " + rating);
 
-    var imgURL = results[j].images.original_still.url;
-    var stillImage = $("<img>").attr("src", imgURL);
+    var gifURL = results[j].images.original.url;
+    
 
-    $("#gif-section").prepend(p , stillImage);
+    var stillURL = results[j].images.original_still.url;
+    
+    var image = $("<img>").attr({"src" : stillURL, "data-still": stillURL, "data-animate" : gifURL, "data-state": "still"});
+    
+
+    $("#gif-section").append(p , image);
     }
     
 });
+};
+
+
+function compare(){
+
+if ($(this).attr("src")===$(this).attr("data-still")) {
+
+    $(this).attr("src", $(this).attr("data-animate"));
 }
+else{
+    $(this).attr("src", $(this).attr("data-still"));
+}
+
+};
+
+$("#form-section").on("click", function(){
+
+});
+
+
 
 initialButtons();
 
 $(document).on("click", ".initial-buttons", searchGiphy);
+
+$(document).on("click", "img", compare)
 
 
 
