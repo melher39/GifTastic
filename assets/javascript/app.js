85ac1eeb7dd7d2524eb5763ca3fa37451a3c0a74
Melvin Hernandez <melher39@gmail.com>
2018-11-09 14:19:09 -0600

    still pictures and ratings show up on screen respective to each button click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,8 @@ var topics = ["Chris Cole", "Shane O'neill", "Mike Mo Capaldi", "Nyjah Juston", 
 "Rodney Mullen", "Jamie Thomas", "PJ Ladd", "Kickflip", "Ollie", "360 Flip", "Zero Skateboards", "Thrasher Mag"]
 
 // var APIkey = "QPQXb6tfP8EP1P1Ci0aJnPiFOUxjDuj5";
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=QPQXb6tfP8EP1P1Ci0aJnPiFOUxjDuj5&q=";
+
+// var searchTerm;
 
 function initialButtons(){
     for (var i=0; i<topics.length ; i++){
@@ -17,13 +18,39 @@ function initialButtons(){
     }
 };
 
-function 
+function searchGiphy() {
 
+    var buttonValue = $(this).attr("data-filter");
+    console.log(buttonValue);
+    var limit = 10;
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + buttonValue +"&api_key=QPQXb6tfP8EP1P1Ci0aJnPiFOUxjDuj5" + "&limit=" + limit;
+    console.log(queryURL);
 
-$(document).on("click", ".initial-buttons", );
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response){
 
+    var results = response.data;
+    for (var j=0; j<results.length; j++) {
+    var rating = results[j].rating;
+    var p = $("<h6>").text("Rating: " + rating);
+
+    var imgURL = results[j].images.original_still.url;
+    var stillImage = $("<img>").attr("src", imgURL);
+
+    $("#gif-section").prepend(p , stillImage);
+    }
+    
+});
+}
 
 initialButtons();
+
+$(document).on("click", ".initial-buttons", searchGiphy);
+
+
+
 
 
 
